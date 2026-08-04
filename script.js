5ad731e29750741cb11a18afcda08d007e6a880f
johnarlosantiago10 <299834273+johnarlosantiago10@users.noreply.github.com>
2026-08-04 16:41:03 +0800

    Refine birthday website UI and letter page

--- script.js
+++ script.js
@@ -24,3 +24,61 @@ if (menuCloseButton) {
     menuOpenButton.click();
   });
 }
+
+function openLetter() {
+  const envelope = document.querySelector(".letter-intro");
+  const letter = document.querySelector("#letterContent");
+
+  envelope.classList.add("hide");
+
+  setTimeout(() => {
+    envelope.style.display = "none";
+
+    letter.style.display = "flex";
+
+    setTimeout(() => {
+      letter.classList.add("show");
+    }, 50);
+  }, 1000);
+}
+
+function openLetter() {
+  const intro = document.querySelector(".letter-intro");
+  const letter = document.querySelector("#letterContent");
+  const navbar = document.querySelector("#navbar");
+
+  intro.classList.add("hide");
+
+  setTimeout(() => {
+    intro.style.display = "none";
+
+    navbar.style.display = "none";
+
+    letter.style.display = "flex";
+
+    setTimeout(() => {
+      letter.classList.add("show");
+    }, 50);
+  }, 1000);
+}
+
+function backToLetter(event) {
+  event.preventDefault();
+
+  const intro = document.querySelector(".letter-intro");
+  const letter = document.querySelector("#letterContent");
+  const navbar = document.querySelector("#navbar");
+
+  letter.classList.remove("show");
+
+  setTimeout(() => {
+    letter.style.display = "none";
+
+    intro.style.display = "flex";
+    navbar.style.display = "flex"; // o "block" depende sa navbar mo
+
+    setTimeout(() => {
+      intro.classList.remove("hide");
+    }, 50);
+  }, 300);
+}
